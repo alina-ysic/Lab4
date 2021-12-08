ade4b41892e9a700e0308418beeec26efb0c8db2
alina-torr <alina.ysic@gmail.com>
2021-12-08 23:06:01 +0300

    Scripted auto-commit on change (2021-12-08 23:06:01) by gitwatch.sh

--- src/main/java/TestingApp.java
+++ src/main/java/TestingApp.java
@@ -1,7 +1,10 @@
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 
 public class TestingApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("test");
+        ActorRef routerActor = system.actorOf(Props.create(RouterActor.class));
     }
 }
