1d039907020ffddef73bf5015729444d3656b8c6
alina-torr <alina.ysic@gmail.com>
2021-12-09 17:18:52 +0300

    Scripted auto-commit on change (2021-12-09 17:18:52) by gitwatch.sh

--- src/main/java/TestingApp.java
+++ src/main/java/TestingApp.java
@@ -5,6 +5,7 @@ import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.Route;
