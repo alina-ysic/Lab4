a43913e87366c84932c959581e1f6f618c24aaea
alina-torr <alina.ysic@gmail.com>
2021-12-09 12:47:50 +0300

    Scripted auto-commit on change (2021-12-09 12:47:50) by gitwatch.sh

--- src/main/java/TestingApp.java
+++ src/main/java/TestingApp.java
@@ -1,10 +1,30 @@
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 
 public class TestingApp {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("test");
+
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        MainHttp instance = new MainHttp(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+                instance.createRoute(system).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
+
+
         ActorRef routerActor = system.actorOf(Props.create(RouterActor.class));
     }
 }
