e3ff4bb40b679e1565497d4af94dbfaba5bf8e9e
alina-torr <alina.ysic@gmail.com>
2021-12-10 20:19:02 +0300

    Scripted auto-commit on change (2021-12-10 20:19:02) by gitwatch.sh

--- src/main/java/TestingApp.java
+++ src/main/java/TestingApp.java
@@ -29,7 +29,7 @@ public class TestingApp {
     }
 
     public static void main(String[] args) throws IOException {
-        //ActorSystem system = ActorSystem.create("test");
+        ActorSystem system = ActorSystem.create("test");
         //ActorRef routerActor = system.actorOf(Props.create(RouterActor.class));
 
         //final Http http = Http.get(system);
