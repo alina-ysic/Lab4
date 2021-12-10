7e1485ca25c881ffa9e2a5fe47d433893d3a244a
alina-torr <alina.ysic@gmail.com>
2021-12-10 19:26:54 +0300

    Scripted auto-commit on change (2021-12-10 19:26:54) by gitwatch.sh

--- src/main/java/GetRequest.java
+++ src/main/java/GetRequest.java
@@ -1,4 +1,14 @@
+import akka.actor.dsl.Inbox;
+
 public class GetRequest {
 
-    
+    private final String pachageId;
+
+    public GetRequest(String pachageId) {
+        this.pachageId = pachageId;
+    }
+
+    public String getPachageId() {
+        return pachageId;
+    }
 }
