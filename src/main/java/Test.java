d8580de87388e600f64e4ca0109c57f99df41ed9
alina-torr <alina.ysic@gmail.com>
2021-12-10 19:48:43 +0300

    Scripted auto-commit on change (2021-12-10 19:48:43) by gitwatch.sh

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
+
 public class Test {
     private String expectedResult;
     private String testName;
-    private String expectedResult;
+    private ArrayList<Object> params;
 }
