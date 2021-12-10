54a840fac57d4b7c7d7a5a4e5e5e5c1648466145
alina-torr <alina.ysic@gmail.com>
2021-12-10 21:31:36 +0300

    Scripted auto-commit on change (2021-12-10 21:31:36) by gitwatch.sh

--- src/main/java/PostRequest.java
+++ src/main/java/PostRequest.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class PostRequest {
     @JsonProperty("packageId")
     private String packageId;
+    @JsonProperty("jsScript")
     private String jsScript;
     private String functionName;
     ArrayList<Test> tests;
