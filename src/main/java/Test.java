40ff204b7542c489fe5df3b1a996ceefbdfc6c33
alina-torr <alina.ysic@gmail.com>
2021-12-10 21:36:00 +0300

    Scripted auto-commit on change (2021-12-10 21:36:00) by gitwatch.sh

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,3 +1,5 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.ArrayList;
 
 public class Test {
@@ -5,5 +7,11 @@ public class Test {
     private String testName;
     private ArrayList<Object> params;
 
-
+    public Test(@JsonProperty("expectedResult") String expectedResult,
+                       @JsonProperty("testName") String testName,
+                       @JsonProperty("params") ArrayList<Object> params) {
+        this.expectedResult = expectedResult;
+        this.testName = testName;
+        this.params = params;
+    }
 }
