99e95fdcbe293efb517da83197f06c43f08ecee0
alina-torr <alina.ysic@gmail.com>
2021-12-10 20:04:08 +0300

    Scripted auto-commit on change (2021-12-10 20:04:08) by gitwatch.sh

--- src/main/java/ExecuterActor.java
+++ src/main/java/ExecuterActor.java
@@ -1,2 +1,8 @@
-public class ExecuterActor {
+import akka.actor.AbstractActor;
+
+public class ExecuterActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
--- src/main/java/ResultingActor.java
+++ src/main/java/ResultingActor.java
@@ -1,2 +1,8 @@
-public class ResultingActor {
+import akka.actor.AbstractActor;
+
+public class ResultingActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
